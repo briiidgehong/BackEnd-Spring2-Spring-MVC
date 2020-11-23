7417c5307ccb9eac702bbfee1a136a2eb25f0a92
cheepiz92 <73451727+mangozzelli@users.noreply.github.com>
2020-11-23 10:54:08 +0900

    service 객체의 사용(IoC 컨테이너에 담아 ListController에 DI

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,7 +1,9 @@
+package controller;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class indexController implements Controller {
+public class IndexController implements Controller {
     @Override
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
 
--- src/main/java/controller/ListController.java
+++ src/main/java/controller/ListController.java
@@ -1,17 +1,27 @@
 package controller;
 
+import entitiy.Notice;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import service.NoticeService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class ListController implements Controller {
+    private NoticeService noticeService;
+
+    public void setNoticeService(NoticeService noticeService) {
+        this.noticeService = noticeService;
+    }
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mv = new ModelAndView("notice/list");
-        mv.addObject("data", "hello SPRING MVC !!!");
         System.out.println("listController");
+        List<Notice> list = noticeService.getNoticeList("TITLE", "", 1);
+        mv.addObject("list",list);
         return mv;
     }
 }
